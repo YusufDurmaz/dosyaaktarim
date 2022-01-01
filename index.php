8a258ec3169151863f06cc14ce6c7de7f2adb888
YusufDurmaz <84239069+YusufDurmaz@users.noreply.github.com>
2022-01-01 18:00:06 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<title>Dosya gönder</title>
+<center>
+  <form action="dosyaaktarim.php" method="post" enctype="multipart/form-data">
+
+<input type="file" name="dosya"/>
+<input type="submit" name="gönder" value="YÜKLE" />
+</form>
+</center>
